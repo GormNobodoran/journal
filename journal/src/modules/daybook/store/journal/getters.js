b228b0059a4d46df910139cb369041757804f1f4
CodigoMorrazo <ipedrosacip@gmail.com>
2021-11-28 13:57:43 +0100

    Added filter by text to the entry list

--- journal/src/modules/daybook/store/journal/getters.js
+++ journal/src/modules/daybook/store/journal/getters.js
@@ -1,8 +1,12 @@
-export const getEntriesByTerm = (state) => {
-   return state;
+export const getEntriesByTerm = (state) => (term) => {
+   if('' === term) {
+      return state.entries;
+   }
+
+   return state.entries.filter( entry => entry.text.toLowerCase().includes(term.toLowerCase()) );
 }
 
-export const getEntryById = (state) => {
-    return state;
-}
-
+// export const getEntryById = (state) => (id) => {
+//     return state;
+// }
+//
